f811fcf7c76bcf88d6a7d9cd3d305ea67e7188e6
Quentin Favrie <quentin.favrie@gmail.com>
2013-05-15 15:49:28 +0200

    Container for animated views added

---
+++ Example/UIViewAnimationExplose/UIViewAnimationExplose/AnimationExploseContainer.h
@@ -0,0 +1,13 @@
+//
+//  AnimationExploseContainer.h
+//  UIViewAnimationExplose
+//
+//  Created by Tseho on 15/05/13.
+//  Copyright (c) 2013 Vivien Cormier. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnimationExploseContainer : NSObject
+
+@end
---
+++ Example/UIViewAnimationExplose/UIViewAnimationExplose/AnimationExploseContainer.m
@@ -0,0 +1,13 @@
+//
+//  AnimationExploseContainer.m
+//  UIViewAnimationExplose
+//
+//  Created by Tseho on 15/05/13.
+//  Copyright (c) 2013 Vivien Cormier. All rights reserved.
+//
+
+#import "AnimationExploseContainer.h"
+
+@implementation AnimationExploseContainer
+
+@end
